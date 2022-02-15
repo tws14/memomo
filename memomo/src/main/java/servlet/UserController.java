3f499a35e8257027d769a6e68d3c5cd26d6204c5
tws14 <twswts14@gmail.com>
2022-02-15 23:58:16 +0900

    ログイン機能２

--- memomo/src/main/java/servlet/UserController.java
+++ memomo/src/main/java/servlet/UserController.java
@@ -5,6 +5,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import model.Login;
+import model.user;
+
 import java.io.IOException;
 
 import DAO.UserDAO;
@@ -12,13 +16,16 @@ import DAO.UserDAO;
 @WebServlet("/UserController")
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
+        
+		private UserDAO userdao;
+	   private user theAccount;
 	//初期化
 	public void init() throws ServletException {
 		super.init();
 		
 		try {
-			UserDAO userdao = new UserDAO();
+			UserDAO Userdao = new UserDAO();
+			userdao = Userdao;
 	   } catch(Exception e ) {
 		   throw new ServletException(e);
 	    }
@@ -30,22 +37,48 @@ public class UserController extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//command パラメータ読み込み
-		String theCommand = request.getParameter("command");
-		//nullの場合
-		if(theCommand == null) {
-			theCommand = "RE";
-		}
-		//メソッド分岐
-		switch(theCommand) {
-			case "RE":
-				relogin(request,response);
-				break;
-		}
-
-}
-	private void relogin(HttpServletRequest request, HttpServletResponse response) {
-		// TODO 自動生成されたメソッド・スタブ
+		//リクエストパラメータ取得
+		request.setCharacterEncoding("UTF-8");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		
+	   //入れる用インスタンス
+		user theUser = new user(username, password);
+		boolean result;
+		
+	try {
+		theAccount = userdao.login(theUser);
+		//Loginインスタンス生成
+		Login loginlogic = new Login();
+		 result = loginlogic.execute(theAccount);
+	
+	if(result = true) {
+			gomainpage(request, response);
+		} else {
+		   relogin(request, response);
+		}
+	}catch(Exception e) {
+		e.printStackTrace();
+	}
+}
+	private void gomainpage(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		//セッションインスタンス取得
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("loginUser", theAccount);
+		
+		response.sendRedirect("/main.jsp");
+		
+	}
+	private void relogin(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		
+		//セッションインスタンス取得
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("status", "no user");
+		
+		response.sendRedirect("/login.jsp");
 	}
 }
