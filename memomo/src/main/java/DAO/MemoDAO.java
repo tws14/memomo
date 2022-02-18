9816fa521f4bb7ff1af71a94e9f8a7fcd8e1c036
tws14 <twswts14@gmail.com>
2022-02-19 02:11:20 +0900

    memo model 追加

---
+++ memomo/src/main/java/DAO/MemoDAO.java
@@ -0,0 +1,14 @@
+package DAO;
+
+public class MemoDAO {
+
+	String databasename = "memo_mo";
+	String propaties = "?characterEncoding=UTF-8&severTimezone=JST";
+	String URL = "jdbc:mysql://localhost:3306/" + databasename + propaties;
+	
+	String USER = "ta*****";
+	String PASS = "*********";
+	
+	
+	
+}
